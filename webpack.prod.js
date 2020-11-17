8c88c42bfeece90eda61e37270dff61bc0e8ca0a
余志成 <yuzhicheng@yuzhichengdeMacBook-Pro-2.local>
2020-11-18 00:01:06 +0800

    chore(wepack.prod.js): :art:  优化webpack.prod.js
    优化webpack.prod.js代码 删除没用的注释

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,4 @@
 const path = require('path')
-// const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')

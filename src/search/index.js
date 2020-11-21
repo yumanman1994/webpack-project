4141f06918f61265933fdc1647bb85be9c1d6887
余志成 <yuzhicheng@yuzhichengdeMacBook-Pro-2.local>
2020-11-21 15:41:28 +0800

    feat(all): :sparkles:  use my npm

--- src/search/index.js
+++ src/search/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import add from 'yxm-large-number';
 import small from './images/small.png';
 import yb from './images/yb.jpeg';
 import './search.less';
@@ -9,19 +10,21 @@ class Search extends React.Component {
     super(props);
     this.state = {
       count: 0,
+      num: add('9999999994234234234243243249', '4234234234299999999999'),
     };
   }
 
   render() {
-    const { count, common } = this.state;
+    const { count, common, num } = this.state;
     return (
       <div className="box">
         <div className="span">span test</div>
-
         <button
           type="button"
           onClick={() => {
-            this.setState(prevState => ({ count: prevState + 1 }));
+            this.setState(prevState => ({
+              count: prevState.count + 1,
+            }));
           }}
         >
           click
@@ -34,6 +37,7 @@ class Search extends React.Component {
           <img src={yb} alt="yb" />
         </div>
         <p>{common}</p>
+        <p>{num}</p>
       </div>
     );
   }
